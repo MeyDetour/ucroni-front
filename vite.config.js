1e887bcb0e5905b50186e54cd2ac1a537cd09868
mey <meydetour@gmail.com>
2025-01-29 16:42:42 +0100

    fix : change vite config,cahnge port style : change beackground repeat

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
     server: {
+    allowedHosts:["https://uchronie-frontend.esdlyon.dev","uchronie-frontend.esdlyon.dev"],
       proxy: {
         '/api': {
           target: 'http://localhost:8000',
